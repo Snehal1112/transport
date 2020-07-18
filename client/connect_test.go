eeff4cce49211c7110787e98104b9659225420f2
snehal dangroshiya <snehaldangroshiya@gmail.com>
2020-07-18 17:34:53 +0530

    Added initial unit test

--- client/connect_test.go
+++ client/connect_test.go
@@ -7,14 +7,49 @@ import (
 	"testing"
 )
 
-func TestNewConnection(t *testing.T) {
+func setup() *Connect{
 	ctx := context.Background()
-
 	url := "mongodb+srv://snehal:VXmWJuqjM8CdtzOa@erp-kod8k.mongodb.net/erp?retryWrites=true&w=majority"
-	conn := NewConnection(ctx, url)
-	err := conn.CreateDocument("leptop", bson.D{{"name", "Alice"}})
+	return NewConnection(ctx, url)
+}
+
+func TestNewConnection(t *testing.T) {
+	conn := setup()
+	err := conn.CreateDocument("leptop", bson.D{{"name", "sd"}})
 	if err != nil {
 		log.Println("Error:", err)
 	}
 
+}
+
+func TestConnect_DeleteByID(t *testing.T) {
+	conn := setup()
+	_, err := conn.DeleteByID("leptop", "5f120b174078a2f86e63fef6")
+	if err != nil {
+		log.Println("Error:", err)
+	}
+}
+
+func TestConnect_FindByID(t *testing.T) {
+	conn := setup()
+	_, err := conn.FindByID("leptop", "5f1270b5e45f4cb2bfa4ac19")
+	if err != nil {
+		log.Println("Error:", err)
+	}
+}
+
+func TestConnect_FindOne(t *testing.T) {
+	conn := setup()
+	_, err := conn.FindOne("leptop", bson.D{{"name","sd"}})
+	if err != nil {
+		log.Println("Error:", err)
+	}
+}
+
+func TestConnect_Search(t *testing.T) {
+	conn := setup()
+	_, err := conn.Search("leptop", bson.D{{"name","sd"}}, 0,0)
+	if err != nil {
+		log.Println("Error:", err)
+	}
 }
