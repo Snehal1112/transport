7527c77a63b03357e814f381a50ae69904d37fe5
snehal dangroshiya <snehaldangroshiya@gmail.com>
2020-07-18 17:34:53 +0530

    Added initial unit test

--- client/connect.go
+++ client/connect.go
@@ -3,12 +3,14 @@ package client
 import (
 	"context"
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo/readconcern"
-	"log"
-
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readconcern"
+	"log"
+	"os"
 )
 
 var (
@@ -20,40 +22,66 @@ var (
 type Connect struct {
 	ctx context.Context
 	url string
+
+	log *logrus.Logger
 }
 
 // NewConnection function used to initialize the connection client.
 func NewConnection(ctx context.Context, url string) *Connect {
-	return &Connect{ctx, url}
+	logLevel, _ := logrus.ParseLevel("info")
+
+	con := &Connect{ctx, url, &logrus.Logger{
+		Out: os.Stderr,
+		Formatter: &logrus.TextFormatter{
+			DisableTimestamp: false,
+			FullTimestamp:    true,
+			TimestampFormat:  "02/01/2006 03:04 PM",
+		},
+		Hooks: make(logrus.LevelHooks),
+		Level: logLevel,
+	}}
+
+	con.log.WithFields(logrus.Fields{
+		"url": con.url,
+	}).Info("Request to connect mongoDB server")
+
+	return con
 }
 
-func (conn *Connect) getMongoClient() *mongo.Client {
+func (conn *Connect) getMongoClient() (*mongo.Client, error) {
 	if mgoClient == nil {
 		client, err := mongo.Connect(conn.ctx, options.Client().ApplyURI(conn.url))
 		if err != nil {
-			log.Fatal(err)
+			conn.log.WithFields(logrus.Fields{
+				"url": conn.url,
+			}).Error("Unable to connect mongoDB server")
+			return nil, err
 		}
 		mgoClient = client
 	}
-
-	return mgoClient
+	return mgoClient, nil
 }
 
 // withDatabase function perform an action on given collection.
 func (conn *Connect) withCollection(collection string, fn func(mongo.SessionContext, *mongo.Collection) error) error {
-	client := conn.getMongoClient()
+	client, err := conn.getMongoClient()
+	if err != nil {
+		return err
+	}
 
 	// Create a new Session and SessionContext.
 	opts := options.Session().SetDefaultReadConcern(readconcern.Majority())
 	session, err := client.StartSession(opts)
 	if err != nil {
-		err := fmt.Errorf("Error in creating new session: %w", err)
-		log.Println(err)
+		conn.log.WithFields(logrus.Fields{
+			"collection": collection,
+		}).Info("Problem in creating session")
+		return fmt.Errorf("error in creating new session: %w", err)
 	}
-	ctx := conn.ctx
-	defer session.EndSession(ctx)
+	defer session.EndSession(conn.ctx)
 
-	err = mongo.WithSession(ctx, session, func(sessionCtx mongo.SessionContext) error {
+	err = mongo.WithSession(conn.ctx, session, func(sessionCtx mongo.SessionContext) error {
+		// TODO: insert some before and after hooks.
 		c := client.Database(databaseName).Collection(collection)
 		return fn(sessionCtx, c)
 	})
@@ -76,7 +104,27 @@ func (conn *Connect) CreateDocument(collectionName string, props interface{}) er
 // FindOne function find the first matching document in collection based on given query.
 func (conn *Connect) FindOne(collectionName string, q interface{}) (result bson.M, err error) {
 	query := func(ctx mongo.SessionContext, c *mongo.Collection) error {
-		return c.FindOne(ctx, q).Decode(result)
+		opts := options.FindOne()
+		return c.FindOne(ctx, q, opts).Decode(&result)
+	}
+	find := func() error {
+		return conn.withCollection(collectionName, query)
+	}
+
+	if err = find(); err != nil {
+
+		return nil, err
+	}
+	return result, nil
+}
+
+func (conn *Connect) FindByID(collectionName string, ID string) (result bson.M, err error) {
+	query := func(ctx mongo.SessionContext, c *mongo.Collection) error {
+		objID, err := primitive.ObjectIDFromHex(ID)
+		if err != nil {
+			return fmt.Errorf("invalid Object id %w", err)
+		}
+		return c.FindOne(ctx, bson.D{{"_id", objID}}).Decode(&result)
 	}
 	find := func() error {
 		return conn.withCollection(collectionName, query)
@@ -93,7 +141,19 @@ func (conn *Connect) Search(collectionName string, q interface{}, skip int, limi
 	query := func(ctx mongo.SessionContext, c *mongo.Collection) error {
 		fieldOptions := options.Find().SetSkip(int64(skip)).SetLimit(int64(limit))
 		cursor, err := c.Find(ctx, q, fieldOptions)
-		_ = cursor.Decode(searchResults)
+		if err != nil {
+			return err
+		}
+
+		for cursor.Next(ctx) {
+			var document bson.M
+			err = cursor.Decode(&document)
+			if err != nil {
+				log.Fatal(err)
+			}
+			searchResults = append(searchResults, document)
+		}
+		defer cursor.Close(ctx)
 		return err
 	}
 
@@ -104,6 +164,52 @@ func (conn *Connect) Search(collectionName string, q interface{}, skip int, limi
 	if err = search(); err != nil {
 		return nil, err
 	}
-
 	return searchResults, nil
+}
+
+// DeleteByID function was delete the document record from
+// given collection.
+func (conn *Connect) DeleteByID(collectionName string, id string) (deleteCount int64, err error) {
+	query := func(ctx mongo.SessionContext, c *mongo.Collection) error {
+		var objID primitive.ObjectID
+		objID, err = primitive.ObjectIDFromHex(id)
+		if err != nil {
+			return fmt.Errorf("invalid Object id %w", err)
+		}
+		var result *mongo.DeleteResult
+		result, err = c.DeleteOne(ctx, bson.D{{"_id", objID}}, options.Delete())
+		deleteCount = result.DeletedCount
+		return err
+	}
+	deleteAction := func() error {
+		return conn.withCollection(collectionName, query)
+	}
+	if err = deleteAction(); err != nil {
+		return 0, err
+	}
+	return deleteCount, nil
+}
+
+// UpdateDocByID function update the document by given id
+func (conn *Connect) UpdateDocByID(collection string, id string, data interface{}) (updatedDoc bson.M,err error) {
+	query := func(ctx mongo.SessionContext, c *mongo.Collection) error {
+		var objID primitive.ObjectID
+		objID, err = primitive.ObjectIDFromHex(id)
+		if err != nil {
+			return fmt.Errorf("invalid Object id %w", err)
+		}
+		opts := options.FindOneAndUpdate().SetUpsert(true)
+		err = c.FindOneAndUpdate(ctx, bson.D{{"_id", objID}}, bson.D{{"$set", data}}, opts).Decode(&updatedDoc)
+		return err
+	}
+
+	updateAction := func() error {
+		return conn.withCollection(collection, query)
+	}
+
+	if err = updateAction(); err != nil {
+		return nil , err
+	}
+
+	return updatedDoc, nil
 }
--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.14
 
 require (
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.7.0
+	github.com/stretchr/testify v1.6.1 // indirect
 	go.mongodb.org/mongo-driver v1.3.5
-	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
